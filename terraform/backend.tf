1506a09030d1607824ef0777e794abfc45322d3a
Michael Ilch <michael13ilch@gmail.com>
2026-04-25 10:34:23 +0000

    Add S3 backend and CI/CD workflow

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "predictive-llm-k8s-tfstate-655748577231"
-    key     = "envs/dev/terraform.tfstate"
-    region  = "eu-central-1"
-    encrypt = true
+    bucket       = "predictive-llm-k8s-tfstate-655748577231"
+    key          = "terraform/terraform.tfstate"
+    region       = "eu-central-1"
+    use_lockfile = true
   }
 }
