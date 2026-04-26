940683c71970b539872baee24140a74892f1141a
Michael Ilch <michael13ilch@gmail.com>
2026-04-26 10:38:27 +0000

    Add EBS CSI storage and update CI CD workflow

---
+++ terraform/storage.tf
@@ -0,0 +1,29 @@
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name                = aws_eks_cluster.main.name
+  addon_name                  = "aws-ebs-csi-driver"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+
+  depends_on = [
+    aws_eks_node_group.main
+  ]
+}
+
+resource "local_file" "gp3_storageclass" {
+  filename = "${path.module}/../k8s/generated/gp3-storageclass.yaml"
+
+  content = <<-YAML
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+  name: gp3
+  annotations:
+    storageclass.kubernetes.io/is-default-class: "true"
+provisioner: ebs.csi.aws.com
+volumeBindingMode: WaitForFirstConsumer
+allowVolumeExpansion: true
+parameters:
+  type: gp3
+  fsType: ext4
+YAML
+}
